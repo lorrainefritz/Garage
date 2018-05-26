95dad7e2410aea3afb838ddcdc33d26168afc591
lorrainefritz <lorraine.fritz0@gmail.com>
2018-05-26 19:04:51 +0200

    Ajout de la variable type (String) + des différentes valeurs qu'elle peut prendre à l'enum TypeMoteur

--- src/com/openC/moteur/TypeMoteur.java
+++ src/com/openC/moteur/TypeMoteur.java
@@ -1,5 +1,16 @@
 package com.openC.moteur;
 
 public enum TypeMoteur {
+	
+	
+	DIESEL ("Je suis un DIESEL" ),
+	ESSENCE ("Je suis une ESSENCE" ),
+	HYBRIDE ("Je suis une HYBRIDE" ),
+	ELECTRIQUE ("Je suis un DIESEL" );
+	
+	private String type ="";
+	
+	
+	
 
 }
