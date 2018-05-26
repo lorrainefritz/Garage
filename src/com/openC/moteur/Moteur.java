51048726d44a7b8f6d1a52ac7c856fd97d57fd29
lorrainefritz <lorraine.fritz0@gmail.com>
2018-05-26 18:55:04 +0200

    Ajout de la JavaDoc à la classe Moteur

--- src/com/openC/moteur/Moteur.java
+++ src/com/openC/moteur/Moteur.java
@@ -1,7 +1,24 @@
 package com.openC.moteur;
 
+/**
+ * <b> Moteur est une classe abstraite qui crée un moteur.</b>
+ * <p>
+ * Un moteur est caractérisé par les informations suivantes :
+ * <ul>
+ * <li>Un prix.</li>
+ * <li>Un type.</li>
+ * <li>Un nombre de cylindres.</li>
+ * </ul>
+ * </p>
+ * 
+ * 
+ * 
+ * @author Lorraine
+ * @version 1.0
+ */
+
 public abstract class Moteur {
-	
+
 	private double prix;
 	private TypeMoteur type;
 	private String cylindre;
