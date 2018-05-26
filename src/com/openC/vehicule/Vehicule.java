72a619e7042cbab591b473deb339141416270a0a
lorrainefritz <lorraine.fritz0@gmail.com>
2018-05-26 17:00:38 +0200

    ajout d'informations JavaDoc

--- src/com/openC/vehicule/Vehicule.java
+++ src/com/openC/vehicule/Vehicule.java
@@ -5,6 +5,25 @@ import java.util.ArrayList;
 import com.openC.moteur.Moteur;
 import com.openC.options.Option;
 
+/**
+ * <b> Vehicule est une classe qui permet l'assemblage d'un véhicule.</b>
+ * <p>
+ * Un véhicule est caractérisé par les informations suivantes :
+ * <ul>
+ * <li>Un prix.</li>
+ * <li>Un nom.</li>
+ * <li>Une Marque.</li>
+ * <li>Un Moteur.</li>
+ * <li>Une Liste d'options.</li>
+ * </ul>
+ * </p>
+ * 
+ * 
+ * 
+ * @author Lorraine
+ * @version 1.0
+ */
+
 public class Vehicule {
 
 	private double prix;
