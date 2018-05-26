5311d2c577e431c4b8265f8b2dc1dee668eb7949
lorrainefritz <lorraine.fritz0@gmail.com>
2018-05-26 16:48:58 +0200

    changement du nom de la variable 'name(string)' pour correspondre au diagramme uml => 'nom(string)' + ajout de la variable moteur (Moteur)

--- src/com/openC/vehicule/Vehicule.java
+++ src/com/openC/vehicule/Vehicule.java
@@ -1,5 +1,16 @@
 package com.openC.vehicule;
 
+import java.util.ArrayList;
+
+import com.openC.moteur.Moteur;
+import com.openC.options.Option;
+
 public class Vehicule {
+
+	private double prix;
+	private String nom;
+	private Marque nomMarque;
+	private Moteur moteur;
+	public ArrayList<Option> options = new ArrayList<>();
 
 }
