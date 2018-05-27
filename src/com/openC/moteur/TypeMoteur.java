a29742c6d72023c39b2f83414b2f480d62d8309a
lorrainefritz <lorraine.fritz0@gmail.com>
2018-05-27 17:28:40 +0200

    Ajout d'un constructeur a l'enum TypeMoteur + redéfinition de la méthode toString()

--- src/com/openC/moteur/TypeMoteur.java
+++ src/com/openC/moteur/TypeMoteur.java
@@ -1,27 +1,33 @@
 package com.openC.moteur;
+
 /**
- * <b> TypeMoteur est un enum qui permet de stocker les différents types de moteurs .</b>
+ * <b> TypeMoteur est un enum qui permet de stocker les différents types de
+ * moteurs .</b>
  * <p>
  * Un Type moteur est caractérisé par les informations suivantes :
  * <ul>
- * <li>Un type.</li> 
+ * <li>Un type.</li>
  * </ul>
  * </p>
+ * 
  * @author Lorraine
  * @version 1.0
  */
 public enum TypeMoteur {
-	
-	
-	DIESEL ("Je suis un DIESEL" ),
-	ESSENCE ("Je suis une ESSENCE" ),
-	HYBRIDE ("Je suis une HYBRIDE" ),
-	ELECTRIQUE ("Je suis un DIESEL" );
-	
-	private String type ="";
-	
-	
-	
-	
+
+	DIESEL("Je suis un DIESEL"),
+	ESSENCE("Je suis une ESSENCE"), 
+	HYBRIDE("Je suis une HYBRIDE"), 
+	ELECTRIQUE("Je suis un DIESEL");
+
+	private String type = "";
+
+	private TypeMoteur(String type) {
+		this.type = type;
+	}
+
+	public String toString() {
+		return type;
+	}
 
 }
