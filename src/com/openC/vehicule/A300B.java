68ca1da89e16a6ff4f2c0e042a0eb5d0d1a7daf0
lorrainefritz <lorraine.fritz0@gmail.com>
2018-05-31 12:04:26 +0200

    Ajout sur le modèle de la classe Lagouna, aux classes A300B et D4, d'un constructeur qui retourne la Marque correspondante

--- src/com/openC/vehicule/A300B.java
+++ src/com/openC/vehicule/A300B.java
@@ -1,5 +1,10 @@
 package com.openC.vehicule;
 
 public class A300B extends Vehicule{
+	
+	public A300B () {
+		super();
+		this.setNomMarque(Marque.PIGEO);
+	}
 
 }
--- src/com/openC/vehicule/D4.java
+++ src/com/openC/vehicule/D4.java
@@ -2,4 +2,9 @@ package com.openC.vehicule;
 
 public class D4 extends Vehicule {
 
+	public D4() {
+		super();
+		this.setNomMarque(Marque.TROEN);
+		
+	}
 }
